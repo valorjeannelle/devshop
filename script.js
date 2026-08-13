99d3efce7a19138647b6a31b2ef12a7b627e040d
Jeannelle <valorjeannelle@gmail.com>
2026-08-13 09:51:35 +0200

    ajout des foncion pour le stockage local et la gestion de qunatité d'articles

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//Initialisation du panier
-const panier = [];
+//Initialisation du panier et sauvagarde local
+const panier = JSON.parse(localStorage.getItem("panier")) || [];
 
 //Récuperation des balises via leur id dans le html
 const conteneur = document.querySelector("#produits");
@@ -7,43 +7,110 @@ const compteur = document.querySelector("#compteur");
 const btnPanier = document.querySelector("#btn-panier");
 const contenuPanier=document.querySelector("#contenu-panier");
 const bntCollection=document.querySelector("#collection");
+const fermerPanier = document.querySelector("#fermer-panier");
+const panierSection = document.querySelector("#panier");
 const collections=document.querySelector("#cartes-produit");
 const loader = document.querySelector("#loader");
 const messageErreur = document.querySelector("#erreur");
 
-//Remonter sur la section du panier
-btnPanier.addEventListener("click", () => {
-    contenuPanier.scrollIntoView();
-});
+affichePanier();
+
+//Sauvegarde local
+function sauvegarderPanier() {
+    localStorage.setItem("panier", JSON.stringify(panier));
+}
 
 //Remonter sur la section des produits
 bntCollection.addEventListener("click",()=>{
     collections.scrollIntoView();
 })
+//Ouvertur et fermeture de la barre lateral
+btnPanier.addEventListener("click", () => {
+    panierSection.classList.add("ouvert");
+});
+
+fermerPanier.addEventListener("click", () => {
+    panierSection.classList.remove("ouvert");
+});
 //Affichage du panier 
-function affichePanier(){
-    contenuPanier.innerHTML="";
-    for(let i=0;i<panier.length;i++){
+function affichePanier() {
+    contenuPanier.innerHTML = "";
+
+    for (let i = 0; i < panier.length; i++) {
+        const article = panier[i];
         const carte = document.createElement("article");
+
         carte.innerHTML = `
-        <img src="${panier[i].image}" alt="${panier[i].title}">
-        <h3>${panier[i].title}</h3>
-        <p>${panier[i].price}</p>
-        <p>${panier[i].category}</p>
-    `;
+            <img src="${article.produit.image}" alt="${article.produit.title}">
+            <h3>${article.produit.title}</h3>
+            <p>${article.produit.price} $</p>
 
-    contenuPanier.appendChild(carte);
+            <div class="quantite">
+                <button class="moins">
+                    <img src="images/signe-moins-dune-ligne-en-position-horizontale.png" alt="icon-moins">
+                </button>
+                <span>${article.quantite}</span>
+                <button class="plus">
+                    <img src="images/plus-symbole-noir.png" alt="icon-plus">
+                </button>
+            </div>
+
+            <button class="supprimer">Supprimer</button>
+        `;
+
+        const moins = carte.querySelector(".moins");
+        const plus = carte.querySelector(".plus");
+        const supprimer = carte.querySelector(".supprimer");
+
+        moins.addEventListener("click", () => {
+            if (article.quantite > 1) {
+                article.quantite--;
+            } else {
+                panier.splice(i, 1);
+            }
+            sauvegarderPanier();
+            affichePanier();
+        });
+
+        plus.addEventListener("click", () => {
+            article.quantite++;
+            sauvegarderPanier();
+            affichePanier();
+        });
+
+        supprimer.addEventListener("click", () => {
+            panier.splice(i, 1);
+            affichePanier();
+        });
+
+        contenuPanier.appendChild(carte);
     }
-
+    sauvegarderPanier();
+    compteur.textContent = panier.reduce(
+        (total, article) => total + article.quantite,
+        0
+    );
 }
-
 // Ajouter un produit au panier
 function ajoutPanier(produit) {
-    panier.push(produit);
-    compteur.textContent = panier.length;
+    const produitExistant = panier.find(article => article.produit.id === produit.id);
+
+    if (produitExistant) {
+        produitExistant.quantite++;
+    } else {
+        panier.push({
+            produit: produit,
+            quantite: 1
+        });
+    }
+
+    compteur.textContent = panier.reduce(
+        (total, article) => total + article.quantite,
+        0
+    );
+
     affichePanier();
 }
-
 
 
 // Création  d'une carte produit
